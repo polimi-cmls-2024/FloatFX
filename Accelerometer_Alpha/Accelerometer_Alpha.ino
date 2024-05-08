d8c65d67b4664eb4b0bef5edbd68cdd84890e545
albi <97albi@gmail.com>
2024-05-08 15:21:28 +0200

    added functions

--- Accelerometer_Alpha/Accelerometer_Alpha.ino
+++ Accelerometer_Alpha/Accelerometer_Alpha.ino
@@ -17,8 +17,8 @@ oneG_ADCValue   = 2.45V --> 2.45/5 * 4095 = 2007
 
 
 // SOFTWARE DEFINITIONS
-#define WINDOW_SAMPLES  50
-#define SETTLE_TIME     3000    //Time in ms for the calibration
+#define WINDOW_SAMPLES 100
+#define SETTLE_TIME 3000  //Time in ms for the calibration
 
 
 
@@ -27,12 +27,41 @@ uint16_t xRest = 1352;
 uint16_t yRest = 1352;
 uint16_t zRest = 2007;
 
+const uint8_t PINS[3] = { X_PIN, Y_PIN, Z_PIN };
+uint16_t axisVett[3];
+int8_t rotVett[3];
+
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  readAxis(WINDOW_SAMPLES);
+
+  //detectRotation();
+  
+}
+
+
+void readAxis(uint16_t samples, uint axis) {
+
+  for (uint j = 0; j < samples; j++) {
+    axisVett[axis] = LPF(axisVett[axis], analogRead(PINS[axis]), samples);
+  }
+
+  return;
+}
+
+void readAxis(uint16_t samples) {
+
+  for (uint j = 0; j < samples; j++) {
+    for (uint i = 0; i < 3; i++) {
+      axisVett[i] = LPF(axisVett[i], analogRead(PINS[i]), samples);
+    }
+  }
+
+  return;
 }
 
 
@@ -47,10 +76,24 @@ void calibrateAccelerometer() {
   while ((millis() - samplingStart) < SETTLE_TIME) {
 
     // Low-pass on the read values
-    xRest = (xRest * (WINDOW_SAMPLES - 1) + analogRead(X_PIN)) / WINDOW_SAMPLES;
-    yRest = (yRest * (WINDOW_SAMPLES - 1) + analogRead(Y_PIN)) / WINDOW_SAMPLES;
-    zRest = (zRest * (WINDOW_SAMPLES - 1) + analogRead(Z_PIN)) / WINDOW_SAMPLES;
+    xRest = LPF(xRest, analogRead(X_PIN), WINDOW_SAMPLES);
+    yRest = LPF(yRest, analogRead(Y_PIN), WINDOW_SAMPLES);
+    zRest = LPF(zRest, analogRead(Z_PIN), WINDOW_SAMPLES);
+  }
+
+  for (uint i = 0; i < 3; i++) {
+    rotVett[i] = 0;
   }
 
   //Serial OSC to notify of the completed calibration
+
+  return;
+}
+
+float LPF(float old_avg, float new_val, uint samples) {
+
+  // weighted average of #samples for the given avg and new value
+
+  float new_avg = (old_avg * (samples - 1) + new_val) / samples;
+  return new_avg;
 }
